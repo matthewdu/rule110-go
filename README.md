c8bf59574973ad2876268ea9dbfac5ae848f7b74
Matthew Du <du.matthew@gmail.com>
2015-09-11 01:37:23 -0400

    add image generation

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ In the root of the repo: `go run rule110.go`
 Or optionally you can compile with `go install` and then run the executable in your GOPATH/bin
 
 ## Webapp
-Go to `http://rule110-go.appspot.com/{num rows}`
+### HTML
+Go to `http://rule110-go.appspot.com/html/{num rows}`
 
-For example `http://rule110-go.appspot.com/80`
+For example `http://rule110-go.appspot.com/html/80`
+
+### PNG
+Go to `http://rule110-go.appspot.com/image/{num rows}`
+
+For example `http://rule110-go.appspot.com/image/80`
+
--- rule110.go
+++ rule110.go
@@ -2,6 +2,9 @@ package rule110
 
 import (
   "fmt"
+  "image"
+  "image/color"
+  "image/png"
   "net/http"
   "regexp"
   "strconv"
@@ -19,17 +22,19 @@ var rules = [2][2][2]bool{
 }
 
 func main() {
-  http.HandleFunc("/", Rule110Handler)
+  http.HandleFunc("/image", ImageHandler)
+  http.HandleFunc("/html", Rule110Handler)
   http.ListenAndServe(":3000", nil)
 }
 
 func init() {
-  http.HandleFunc("/", Rule110Handler)
+  http.HandleFunc("/image/", ImageHandler)
+  http.HandleFunc("/html/", Rule110Handler)
 }
 
 func Rule110Handler(w http.ResponseWriter, r *http.Request) {
-  re := regexp.MustCompile("/\\d*")
-  i, err := strconv.Atoi(re.FindString(r.RequestURI)[1:])
+  re := regexp.MustCompile("/html/\\d*")
+  i, err := strconv.Atoi(re.FindString(r.RequestURI)[6:])
   if err != nil {
     fmt.Fprint(w, "Please pass in number of rows")
     return
@@ -88,6 +93,49 @@ func Rule110Handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintln(w, `
         </body>
     </html>`)
+}
+
+func ImageHandler(w http.ResponseWriter, r *http.Request) {
+  re := regexp.MustCompile("/image/\\d*")
+  i, err := strconv.Atoi(re.FindString(r.RequestURI)[7:])
+  if err != nil {
+    fmt.Fprint(w, "Please pass in number of row")
+    return
+  }
+
+  m := image.NewRGBA(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{i, i}})
+//  for y := 0; y < i; y++ {
+//    for x := 0; x < i; x++ {
+//      m.SetRGBA(x, y, color.RGBA{uint8(x), uint8((x + y) / 2), uint8(y), 255})
+//    }
+//  }
+
+  arr := [2][]bool{
+    make([]bool, i),
+    make([]bool, i),
+  }
+  arr[0][i-1] = true
+  arr[1][i-1] = true
+  for row := 1; row < i; row++ {
+    arr[0][i-row-1] = true
+    if row != 0 {
+      for j := i-row; j < i-1; j++ {
+        if j == 0 { continue }
+        arr[1][j] = Rule(arr[0][j-1], arr[0][j], arr[0][j+1])
+      }
+    }
+    for j := range arr[1] {
+      arr[0][j] = arr[1][j]
+      if arr[1][j] {
+        m.SetRGBA(j, row, color.RGBA{0, 0, 0, 255})
+      } else {
+        m.SetRGBA(j, row, color.RGBA{255, 255, 255, 255})
+      }
+    }
+  }
+  w.Header().Set("Content-type", "image/png")
+  w.Header().Set("Cache-control", "public, max-age=259200")
+  png.Encode(w, m)
 }
 
 func Rule(i, j, k bool) bool {
